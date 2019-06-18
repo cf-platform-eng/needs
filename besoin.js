e8b21eabf1a1d86229ba32425e9c3956e685b323
Pete Wall pwall@pivotal.io <pwall@pivotal.io>
2019-06-18 13:08:31 -0400

    MVP

--- besoin.js
+++ besoin.js
@@ -2,12 +2,33 @@
 
 const fs = require("fs")
 const path = require("path")
+const { promisify } = require("util")
+const readFile = promisify(fs.readFile)
+
 const defaultNeedsFile = path.join(__dirname, "environmentvars.json")
-// const defaultNeedsTypes = path.join(__dirname, "lib", "needs")
 
 const Needs = require("./lib/needs.js")
 const Types = require("./lib/types.js")
 const types = new Types()
+
+function needsToJSON(needs) {
+  return JSON.stringify(needs.map((need) => need.data))
+}
+
+async function loadNeedsFromFile(file) {
+  let needs = new Needs()
+
+  try {
+    let raw = await readFile(file)
+    let data = JSON.parse(raw)
+    await needs.load(data, types)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  return needs
+}
 
 require("yargs")
   .option("file", {
@@ -20,37 +41,35 @@ require("yargs")
       type: "boolean",
       describe: "only list unsatisfied needs"
     })
-  }, (args) => {
-    fs.readFile(args.file, (err, raw) => {
-      if (err) {
-        console.error(err)
-        return 1
-      }
+  }, async (args) => {
+    let needs = await loadNeedsFromFile(args.file)
 
-      let data = JSON.parse(raw)
-      let needs = new Needs()
-      needs.load(data, types)
-      console.log(JSON.stringify(needs.data()))
-    })
+    if (!args.unsatisfied) {
+      console.log(needsToJSON(needs.needs))
+      return
+    }
+
+    try {
+      let unsatisfied = await needs.getUnsatisfied()
+      console.log(needsToJSON(unsatisfied))
+    } catch (err) {
+      console.error("error: ", err)
+      process.exit(1)
+    }
   })
-  .command("check", "check current needs", yargs => yargs, (args) => {
-    fs.readFile(args.file, async (err, raw) => {
-      if (err) {
-        console.error(err)
-        return 1
-      }
+  .command("check", "check current needs", yargs => yargs, async (args) => {
+    let needs = await loadNeedsFromFile(args.file)
 
-      let data = JSON.parse(raw)
-      let needs = new Needs()
-      needs.load(data, types)
-      needs.getUnsatisfied().then((unsatisfied) => {
-        if (unsatisfied.length > 0) {
-          console.log(JSON.stringify(unsatisfied))
-        }
-      }).catch((err) => {
-        console.error(err)
-      })
-    })
+    try {
+      let unsatisfied = await needs.getUnsatisfied()
+      if (unsatisfied.length > 0) {
+        console.error("Some needs were unsatisfied:")
+        console.log(needsToJSON(unsatisfied))
+      }
+    } catch (err) {
+      console.error("error: ", err)
+      process.exit(1)
+    }
   })
   .command("types", "list installed types", () => {
     types.all().sort().forEach((type) => {
@@ -64,95 +83,8 @@ require("yargs")
       console.log(type.info)
     } else {
       console.error(`Type "${typeName}" does not exist`)
-      // TODO: return bad
+      process.exit(1)
     }
   })
   .help()
   .argv
-
-// let check = require("./lib/check.js")
-// let Needs = require("./lib/needs.js")
-// let needs = new Needs(() => {
-//   if (command == "list") {
-//     needs.load((err, myneeds) => {
-//       if (err) {
-//         console.error("Failed to read needs file", err)
-//         process.exit(1)
-//       }
-
-//       let output = myneeds.map(myneed => myneed.data)
-//       console.log(JSON.stringify(output))
-//       process.exit(0)
-//     })
-//   }
-
-//   else if (command == "check") {
-//     needs.load((err, myneeds) => {
-//       if (err) {
-//         console.error("Failed to read needs file", err)
-//         process.exit(1)
-//       }
-
-//       check(myneeds, (err, remaining) => {
-//         if (err) {
-//           console.log("Failed to check needs", err)
-//           process.exit(1)
-//         }
-        
-//         if (remaining.length > 0) {
-//           console.error("There are still unsatisfied needs:")
-//           let output = remaining.map(myneed => myneed.data)
-//           console.log(JSON.stringify(output))  
-//           process.exit(1)
-//         }
-//         process.exit(0)
-//       })
-//     })
-//   }
-
-//   else if (command == "unsatisfied") {
-//     needs.load((err, myneeds) => {
-//       if (err) {
-//         console.error("Failed to read needs file", err)
-//         process.exit(1)
-//       }
-
-//       check(myneeds, (err, remaining) => {
-//         if (err) {
-//           console.log("Failed to check needs", err)
-//           process.exit(1)
-//         }
-
-//         let output = remaining.map(myneed => myneed.data)
-//         console.log(JSON.stringify(output))
-//         process.exit(0)
-//       })
-//     })
-//   }
-
-//   else if (command == "types") {
-//     let types = needs.needTypes()
-//     types.forEach((type) => {
-//       console.log(type)
-//     })
-//     process.exit(0)
-//   }
-
-//   else if (command == "info") {
-//     let type = process.argv[3]
-//     let need = needs.needsClass(type)
-//     if (!need) {
-//       console.error("No need with type " + type + " exists")
-//       process.exit(1)
-//     }
-  
-//     console.log(need.info)
-//     process.exit(0)
-//   }
-
-//   else {
-//     console.error("invalid command")
-//     process.exit(1)
-//   }
-// })
-
