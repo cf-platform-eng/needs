f9dab627f7ef8d9e8cdce1f529e0099bd9a28252
Serafima Ostrovskaya <sostrovskaya@pivotal.io>
2021-05-06 15:30:26 -0400

    Add -alpine release

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 const { exec } = require("pkg")
-exec([".", "--targets", "latest-linux-x64,latest-macos-x64", "--out-path", "./build"])
+exec([".", "--targets", "latest-linux-x64,latest-macos-x64,latest-alpine-x64", "--out-path", "./build"])
